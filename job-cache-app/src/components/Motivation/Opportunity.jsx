436b33aa3883041cad8e5012d29443cd12ec7ea0
mikegag <Michael.gagliardi1@hotmail.com>
2024-05-07 10:22:32 -0400

    updated useEffect dependency to fix infinite loop

--- job-cache-app/src/components/Motivation/Opportunity.jsx
+++ job-cache-app/src/components/Motivation/Opportunity.jsx
@@ -80,7 +80,7 @@ export default function Opportunity() {
             clearInterval(intervalId)
             viewedOpportunity.current = false
         }
-    }, [])
+    }, [viewedOpportunity.current])
 
     const positionTitleStyling = {
         fontSize:"0.9rem",
